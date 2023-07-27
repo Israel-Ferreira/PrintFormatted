78230b30ac04de5d01beb4fe60d0635ce2165247
Israel-Ferreira <israelfsouza@hotmail.com>
2023-07-26 23:20:22 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace PrintFormatted
+{
+    internal class Program
+    {
+
+
+        static void FormatAndPrintAllNumbersInRange(int num)
+        {
+            for(int itNumber=  1; itNumber <= num; itNumber++)
+            {
+                string hexNumber = itNumber.ToString("X");
+                string octalNumber = Convert.ToString(itNumber, 8);
+
+                Console.WriteLine($"{itNumber}  {octalNumber}  {hexNumber}");
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.Write("\nInforme um número: ");
+            int numero = int.Parse(Console.ReadLine()!);
+
+
+            FormatAndPrintAllNumbersInRange(numero);
+        }
+    }
+}
